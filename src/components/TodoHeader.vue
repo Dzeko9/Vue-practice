e1aa637fb461d1dbc0ca46e67fcbe6897f3aa8a6
Dzeko9 <jhpark9007@gmail.com>
2024-03-26 14:07:26 +0900

    test

---
+++ src/components/TodoHeader.vue
@@ -0,0 +1,15 @@
+<template lang="">
+    <header>
+        <h1>To Do LIST!</h1>
+    </header>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="">
+    
+</style> 
---
+++ src/components/TodoInput.vue
@@ -0,0 +1,33 @@
+<template lang="html">
+    <div>
+        <input type="text" v-model="newTodoItem" v-on:keyup.enter="addTodo">
+        <button v-on:click="addTodo">추가</button>
+    </div>
+</template>
+
+<script lang="javascript">
+export default {
+    data() {
+        return {
+            newTodoItem: ''
+        }
+    },
+    methods: { 
+        addTodo() {
+            if (this.newTodoItem !== '') {
+                var value = this.newTodoItem && this.newTodoItem.trim();
+                this.$emit('addTodo', value);
+                this.clearInputbox();
+            }
+        },
+
+        clearInputbox() {
+            this.newTodoItem = '';
+        }
+    }
+}
+</script>
+
+<style lang="css">
+    
+</style>
